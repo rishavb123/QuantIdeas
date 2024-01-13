51357208b314ff19cbf43dbb92b1cafe936ad513
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-01-13 17:40:49 -0500

    created project framework

---
+++ notebooks/init.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sys.path.insert(1, "../src")
+
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from util import *
---
+++ src/util.py
@@ -0,0 +1,2 @@
+def print_line(len=100, character="-"):
+    print(character * len)
