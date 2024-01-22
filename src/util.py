5799b920fdabb6ef5d7752795263fc72786190a9
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-01-21 19:14:25 -0500

    ran formatter

--- src/util.py
+++ src/util.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def print_line(len=100, character="-"):
     print(character * len)
 
-def plot_returns(returns_df):
+
+def plot_returns(returns_df, title="Returns"):
     ax = returns_df.plot.bar(color=np.where(returns_df > 0, "g", "r"))
     ax.set_xticks([])
-    plt.title("Returns")
+    plt.title(title)
     plt.xlabel("Time")
     plt.ylabel("Returns")
     plt.show()
