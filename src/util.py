650382ed7c09a4e86720be0e65b5d481186ab827
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-01-21 19:12:30 -0500

    added returns plotting util

--- src/util.py
+++ src/util.py
@@ -1,2 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def print_line(len=100, character="-"):
     print(character * len)
+
+def plot_returns(returns_df):
+    ax = returns_df.plot.bar(color=np.where(returns_df > 0, "g", "r"))
+    ax.set_xticks([])
+    plt.title("Returns")
+    plt.xlabel("Time")
+    plt.ylabel("Returns")
+    plt.show()
